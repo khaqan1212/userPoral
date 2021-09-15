d48e94bb56831b5742d1607fd1131473cf137db2
khaqan1212 <khaqan12@live.com>
2021-09-15 21:39:15 +0500

    3rd commit

--- force-app/main/default/lwc/userPortalTab/userPortalTab.js
+++ force-app/main/default/lwc/userPortalTab/userPortalTab.js
@@ -5,6 +5,11 @@ import getAccountInfo from '@salesforce/apex/BuildingAccountController.getAccoun
 import getTermField from '@salesforce/apex/BuildingAccountController.getTermField';
 import getBuildingAddressField from '@salesforce/apex/BuildingAccountController.getBuildingAddressField';
 import getFreeRentField from '@salesforce/apex/BuildingAccountController.getFreeRentField';
+import getTIsField from '@salesforce/apex/BuildingAccountController.getTIsField';
+import getCriticalDates from '@salesforce/apex/BuildingAccountController.getCriticalDates';
+import getLeases from '@salesforce/apex/BuildingAccountController.getLeases';
+import getIndividualLeaseDocuments from '@salesforce/apex/BuildingAccountController.getIndividualLeaseDocuments';
+import urlsOfLeasesMap from '@salesforce/apex/BuildingAccountController.urlsOfLeasesMap';
 
 export default class BuildingInformation extends LightningElement {
   @api recordId
@@ -14,6 +19,11 @@ export default class BuildingInformation extends LightningElement {
   @track termField
   @track buildingAddress
   @track freeRentField
+  @track TIsField
+  @track criticalDates
+  @track leases
+  @track individualLeaseDocuments
+  @track urls = []
   
 
 
@@ -77,5 +87,72 @@ export default class BuildingInformation extends LightningElement {
         }
   }
 
+  @wire(getTIsField,{recordId:'$recordId'})
+  wiredTIsField({error, data}){
+      if(data){
+          this.TIsField = data;
+          this.error = undefined;
+          console.log(this.TIsField);
+      } else if(error){
+          this.error = error;
+          this.TIsField = undefined;
+        }
+  }
+
+  @wire(getCriticalDates,{recordId:'$recordId'})
+  wiredcriticalDates({error, data}){
+      if(data){
+          this.criticalDates = data;
+          this.error = undefined;
+          console.log(this.TIsField);
+      } else if(error){
+          this.error = error;
+          this.criticalDates = undefined;
+        }
+  }
   
+  @wire(getLeases,{recordId:'$recordId'})
+  wiredLeases({error, data}){
+      if(data){
+          this.leases = data;
+          this.error = undefined;
+          console.log(this.leases);
+      } else if(error){
+          this.error = error;
+          this.leases = undefined;
+        }
+  }
+
+  @wire(getIndividualLeaseDocuments,{recordId:'$recordId'})
+  wiredIndividualLeases({error, data}){
+      if(data){
+          this.individualLeaseDocuments = data;
+          this.error = undefined;
+          console.log(this.individualLeaseDocuments);
+      } else if(error){
+          this.error = error;
+          this.individualLeaseDocuments = undefined;
+        }
+  }
+
+  @wire(urlsOfLeasesMap,{recordId:'$recordId'})
+  wiredURLs({error, data}){
+      if(data){
+        for(let key in data) {
+            // Preventing unexcepted data
+            if (data.hasOwnProperty(key)) { // Filtering the data in the loop
+                this.urls.push({key: key, value: data[key]});
+            }
+        }
+          this.error = undefined;
+          console.log('URLS: '+this.urls);
+      } else if(error){
+          this.error = error;
+          this.urls = undefined;
+        }
+  }
+
+  getURL(id) {
+      return '/lightning/r/Account/' + id +'/view';
+  }
 }
